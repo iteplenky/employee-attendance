6751e8d07c3fe74d3219dded35b19548e2b00094
iteplenky <iteplenky@gmail.com>
2025-02-25 20:49:38 +0500

    Make table creation idempotent

--- database/migrations/20250225023214_initial_tables_users_and_schedules.sql
+++ database/migrations/20250225023214_initial_tables_users_and_schedules.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     tg_id BIGINT UNIQUE NOT NULL,
     iin VARCHAR(12) UNIQUE NOT NULL
 );
 
-CREATE TABLE schedules (
+CREATE TABLE IF NOT EXISTS schedules (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
     start_time TIME NOT NULL,
